52835227b8ebd84e1f7633a5e2e0e4d54fe6c229
fsgom <fsg_tmp@hotmail.com>
2016-11-21 16:37:19 -0600

    Creating the components which will act within the library

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
+var React=require("react");
+var ReactDOM = require('react-dom');
 class ReactPagination extends React.Component{
-  
+  render(){
+    return(<section><ListItem/><TabsPage /></section>)
+  }
 }
+
+class ListItem extends React.Component{
+  render(){
+    return(<table>
+      <thead>
+        <td>First attribute</td>
+      </thead>
+      <tbody>
+        <tr>
+          <td>First value</td>
+        </tr>
+      </tbody>
+      </table>)
+  }
+}
+
+class TabsPage extends React.Component{
+  render(){
+    return(<ul><li>1</li></ul>)
+  }
+}
+
+ReactDOM.render(
+  <ReactPagination />,
+  document.getElementById("story-app")
+);
